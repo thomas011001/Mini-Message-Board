88f96e6b5f64c0583247e5f47437027b51b07ca8
thomas <thomasyacoub321@gmail.com>
2025-06-10 06:57:36 +0300

    v0

--- app.js
+++ app.js
@@ -2,10 +2,23 @@ const path = require("node:path");
 const express = require("express");
 const app = express();
 
+const {
+  getAllMessages,
+  createMessage,
+} = require("./controllers/messageController");
+
 require("dotenv").config();
 const PORT = require("process").env.PORT;
 
+const assetsPath = path.join(__dirname, "public");
+app.use(express.static(assetsPath));
+
+app.use(express.urlencoded());
+
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+
+app.get("/", getAllMessages);
+app.post("/new", createMessage);
 
 app.listen(PORT, () => console.log("started the server on: " + PORT));
